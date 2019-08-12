74ba7e9cd3466b43174faa18a3b9b6e9d2dc65ab
darkolol9 <53996212+darkolol9@users.noreply.github.com>
2019-08-12 09:23:31 +0300

    Add files via upload

---
+++ learning_imagine_capture.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+from PIL import ImageGrab
+
+img = ImageGrab.grab()    #grabs a screenshot
+#img_np = np.array(img)   //transforms the image into a format that cv can accept (an array of pixels)
+print(img.show())        #img.show() to open the img to the user
+
+#image_np = np.array(img)
+
