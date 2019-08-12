8cedb610435ef8a308240ad9c938a41cfca6fdd0
darkolol9 <whinedine8@gmail.com>
2019-08-12 09:29:14 +0300

    Update learning_imagine_capture.py

--- learning_imagine_capture.py
+++ learning_imagine_capture.py
@@ -2,6 +2,9 @@ import numpy as np
 import cv2
 from PIL import ImageGrab
 
+
+print("this is a Github commit test...")
+
 img = ImageGrab.grab()    #grabs a screenshot
 #img_np = np.array(img)   //transforms the image into a format that cv can accept (an array of pixels)
 print(img.show())        #img.show() to open the img to the user
